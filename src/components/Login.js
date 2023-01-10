0bbb5dfcefcec24dbd121ab1ee33aaba8778fcb4
djszal <szalank.dj@gmail.com>
2023-01-10 13:08:33 -0500

    Fixed merge errors

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,3 @@
-import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom'
-import "./Login.css";
-
-
-const Login = () => {
-    const [usernameLogin, setUsernameLogin] = useState("");
-    const [passwordLogin, setPasswordLogin] = useState("");
-
-    return(
-        <>
-        <div className="login">
-            <h3 className="title">Login</h3>
-            <form className="login">
-                <input value={usernameLogin} type="text" placeholder="username" minLength={3} onChange={(e) => setUsernameLogin(e.target.value)}></input> 
-                <input value={passwordLogin} type="password" placeholder="password" minLength={3} onChange={(e) => setPasswordLogin(e.target.value)}></input>
-                <button type="submit">Login</button>
-                <Link to="/register">Don't have an account? Register Here</Link>
-            </form>
-        </div>
-    </>
-    )
-    
-}
-
-export default Login;
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
